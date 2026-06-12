66a810d80c88c4d7d6b0a4c6320cf1ccb49ea2bf
jefersontortoza01-spec <jefersontortoza01@gmail.com>
2026-06-12 00:51:16 -0400

    Delete app.py

--- app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import cv2
-import tempfile
-import os
-from detection_engine import detect_threats # <-- ¡ESTO DEBE COINCIDIR EXACTAMENTE!
-
-# Y más abajo, cuando usas la función, debe ser:
-# resultado = detect_threats(frame)
-
