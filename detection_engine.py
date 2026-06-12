e5dde4b4db3c9672e1fd297b0c5a1402b37251a0
jefersontortoza01-spec <jefersontortoza01@gmail.com>
2026-06-12 00:01:06 -0400

    Update detection_engine.py

--- detection_engine.py
+++ detection_engine.py
@@ -1,3 +1,5 @@
-def detect_threats(frame):
-    # Lógica de visión artificial
-    return "Seguro"
+def procesar_deteccion(frame):
+    # Aquí irá tu lógica de visión artificial en el futuro
+    # Por ahora, simplemente devolvemos el frame tal cual
+    return frame
+
